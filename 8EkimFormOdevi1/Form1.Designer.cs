5c4827995ed022df6cd0aad52dc41619ee70e290
CENK KOC <2310206256@ogrenci.karabuk.edu.tr>
2024-10-16 14:29:20 +0300

    Proje dosyası ekle.

---
+++ 8EkimFormOdevi1/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace _8EkimFormOdevi1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tahminKutusuTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tahminKutusuTextBox
+            // 
+            this.tahminKutusuTextBox.Location = new System.Drawing.Point(331, 124);
+            this.tahminKutusuTextBox.Name = "tahminKutusuTextBox";
+            this.tahminKutusuTextBox.Size = new System.Drawing.Size(179, 20);
+            this.tahminKutusuTextBox.TabIndex = 0;
+            this.tahminKutusuTextBox.TextChanged += new System.EventHandler(this.tahminKutusuTextBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Location = new System.Drawing.Point(169, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter a number :";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(382, 174);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 64);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Check";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tahminKutusuTextBox);
+            this.Name = "Form1";
+            this.Text = "Tahmin Oyunu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tahminKutusuTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ 8EkimFormOdevi1/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace _8EkimFormOdevi1
+{
+    public partial class Form1 : Form
+    {
+        int randomNum;
+        int counter = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            randomNum = random.Next(0,100)+1;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            for(; ; )
+            {
+                if (int.TryParse(tahminKutusuTextBox.Text , out int guess))
+                
+
+            if ( guess == randomNum)
+            {
+                MessageBox.Show("Correct guess", "RESULT");
+                break;
+            }
+            else
+            {
+                if (guess > randomNum)
+                        {
+                            MessageBox.Show("Enter less value");
+                        }
+                else
+                        {
+                            MessageBox.Show("Enter high value");
+                        }
+                        counter++;
+                MessageBox.Show($"You have {5-counter} chacne", "RESULT");
+                tahminKutusuTextBox.Clear();
+                if (counter == 5)
+                    {
+                            MessageBox.Show($"You lost the game . The correct number is {randomNum}", "RESULT");
+                            this.Close();
+                            break;
+                    }
+                    return;
+                
+            }
+            }
+        }
+
+        private void tahminKutusuTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
